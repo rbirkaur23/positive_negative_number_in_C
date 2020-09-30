547dc94ee0b366949701c1482b7e42c0ee8ad77c
AvikalGupta <59913494+AvikalGupta@users.noreply.github.com>
2020-09-30 22:18:12 +0530

    Perfect program to check Positive or Negative
    The given program had a bug that if any user enters 0 as a int input then program would not give any output but it should, because Zero is not a positive or negative number.

--- posneg.c
+++ posneg.c
@@ -2,16 +2,14 @@
 #include<conio.h>
 void main()
 {
-int n;
-print("Enter n");
-scanf("%d",&n);
-if(n>0)
-{
-printf("Positive");
-}
-else
-{
-printf("Negative");
-}
-getch();
+  int n;
+  print("Enter n");
+  scanf("%d",&n);
+  if(n>0)
+    printf("Positive");
+  else if(n<0)
+    printf("Negative");
+  else
+    printf("Neither Positive Nor Negetive");
+  getch();
 }
