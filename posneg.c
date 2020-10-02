75938cb31e8df267ff50e24d458a2ce6099c0bbc
skaur01 <72157116+skaur01@users.noreply.github.com>
2020-10-02 18:30:29 +0530

    Updated posneg.c

--- posneg.c
+++ posneg.c
@@ -3,13 +3,13 @@
 void main()
 {
   int n;
-  print("Enter n");
+  printf("Enter n");
   scanf("%d",&n);
   if(n>0)
     printf("Positive");
   else if(n<0)
     printf("Negative");
   else
-    printf("Neither Positive Nor Negetive");
+    printf("Neither Positive Nor Negative");
   getch();
 }
